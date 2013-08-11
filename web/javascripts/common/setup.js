ddec767920fe6460a447d164ca670b33de57853f
Tobias Hourst <tobiashourst@gmail.com>
2013-08-11 08:50:59 +0200

    isIE js

--- web/javascripts/common/setup.js
+++ web/javascripts/common/setup.js
@@ -6,39 +6,45 @@ $(document).ready( function() {
 	// javascript enabled
 	$('body').removeClass('js-disabled');
 	
+	// is internet explorer
+	var isIE = /*@cc_on!@*/false;
+	
 	// foundation
 	$(document).foundation();
 	
-	// navigation effects
-	var longuest_duration = 0;
-	$('.navigation li:not(.active)').each(function(){
-		var duration = Math.floor((Math.random() * 1500) + 500);
-		$(this).animate({left: 0}, duration, function(){});
+	if (!isIE) {
 		
-		if (duration > longuest_duration) {
-			longuest_duration = duration;
-		}
-	});
-	
-	// process effects
-	var duration = 1000;
-
-	// onload opacity
-	$('.container .process .box').each(function(){	
-		$(this).animate({
-			opacity: 1
-			}, duration, function () {}
-		);
+		// navigation effects
+		var longuest_duration = 0;
+		$('.navigation li:not(.active)').each(function(){
+			var duration = Math.floor((Math.random() * 1500) + 500);
+			$(this).animate({left: 0}, duration, function(){});
+			
+			if (duration > longuest_duration) {
+				longuest_duration = duration;
+			}
+		});
 		
-		duration += 400;
-	});
+		// process effects
+		var duration = 1000;
 	
-	setTimeout("$('.navigation li:not(.active):not(.client_area)').hover(function(){" +
-			"$(this).stop().animate({width:'100%', backgroundColor: '#B8383D'}, 500);" +
-			"$('a', this).stop().animate({color:'#fff'}, 700);" +
-			"}, function(){ " +
-			"$(this).stop().animate({width:'90%', backgroundColor: '#efefef'}, 500); " +
-			"$('a', this).stop().animate({color:'#756868'}, 700);" +
-			"});", longuest_duration);
+		// onload opacity
+		$('.container .process .box').each(function(){	
+			$(this).animate({
+				opacity: 1
+				}, duration, function () {}
+			);
+			
+			duration += 400;
+		});
+		
+		setTimeout("$('.navigation li:not(.active):not(.client_area)').hover(function(){" +
+				"$(this).stop().animate({width:'100%', backgroundColor: '#B8383D'}, 500);" +
+				"$('a', this).stop().animate({color:'#fff'}, 700);" +
+				"}, function(){ " +
+				"$(this).stop().animate({width:'90%', backgroundColor: '#efefef'}, 500); " +
+				"$('a', this).stop().animate({color:'#756868'}, 700);" +
+				"});", longuest_duration);
+	}
 
 });
